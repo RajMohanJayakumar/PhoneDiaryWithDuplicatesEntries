585365647f090cb116808eec033ae5e746723179
unknown <rajmohan2695@>
2019-10-16 20:13:07 +0530

    Added POJO class

---
+++ Pojo.java
@@ -0,0 +1,29 @@
+public class Pojo {
+    private String mName;
+    private String mEmail;
+    private String mPhonenumber;
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmEmail() {
+        return mEmail;
+    }
+
+    public void setmEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public String getmPhonenumber() {
+        return mPhonenumber;
+    }
+
+    public void setmPhonenumber(String mPhonenumber) {
+        this.mPhonenumber = mPhonenumber;
+    }
+}
