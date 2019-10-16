8c5bd1dc0d6ce33397fe867b47d20357f536b20e
unknown <rajmohan2695@>
2019-10-16 20:13:43 +0530

    Major Changes, Working on Serialization

--- PhoneDiary.java
+++ PhoneDiary.java
@@ -1,16 +1,18 @@
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-public class PhoneDiary {
+import java.util.TreeSet;
 
-    String mName;
-    String mEmail;
-    String mPhonenumber;
+public class PhoneDiary implements Serializable {
+
+    Pojo p = new Pojo();
     String mTemp;
 
     Map<String,String> mPhoneBook = new HashMap<>();
+    TreeSet<String> mOrdered = new TreeSet<>();
     Scanner scan = new Scanner(System.in);
-    public void run() {
+    public void run() throws Exception{
 
         while (true) {
             System.out.println("--------------------------");
@@ -30,9 +32,19 @@ public class PhoneDiary {
                     this.search();
                     break;
                 }
-                case "3":
-                case "4":
-                case "5": break;
+                case "3":{
+                    this.showAll();
+                    break;
+                }
+                case "4":{
+                    break;
+//                    this.edit();
+//                    break;
+                }
+                case "5": {
+                    this.remove();
+                    break;
+                }
                 case "6":
                     System.out.println("Exiting Windows..");
                     return;
@@ -43,25 +55,71 @@ public class PhoneDiary {
         }
     }
 
-    public void add(){
+    public void add() throws  Exception{
         scan.nextLine();
         System.out.println("Enter the Name");
-        mName = scan.next();
+        p.setmName(scan.nextLine());
         System.out.println("Enter the Phone Number");
-        mPhonenumber = scan.next();
+        p.setmPhonenumber(scan.next());
         System.out.println("Enter the Email Address");
-        mEmail = scan.next();
-        mTemp=mName+"&/&"+mPhonenumber+"&/&"+mEmail;
-        mPhoneBook.put(mName,mTemp);
+        p.setmEmail(scan.next());
+        mTemp=p.getmName()+"&/&"+p.getmPhonenumber()+"&/&"+p.getmEmail();
+        mPhoneBook.put(p.getmName(),mTemp);
+        mOrdered.add(p.getmName());
+
     }
 
     public void search(){
         System.out.println("Enter the Name to Search");
-        mTemp = mPhoneBook.get(scan.next());
+        Scanner scan2 = new Scanner(System.in);
+        mTemp = mPhoneBook.get(scan2.nextLine());
         String lDetails[] = mTemp.split("&/&");
         System.out.println("Name : "+lDetails[0]);
         System.out.println("Phone Number : "+lDetails[1]);
         System.out.println("Email : "+lDetails[2]);
+
+    }
+
+    public void showAll(){
+        for (String i: mOrdered) {
+            String lDetails[] = mPhoneBook.get(i).split("&/&");
+            System.out.println("Name : "+lDetails[0]);
+            System.out.println("Phone Number : "+lDetails[1]);
+            System.out.println("Email : "+lDetails[2]);
+        }
+    }
+
+    public void remove(){
+        System.out.println("Enter the Name to be removed");
+        Scanner scan3 = new Scanner(System.in);
+        mTemp = scan3.nextLine();
+        mPhoneBook.remove(mTemp);
+        mOrdered.remove(mTemp);
+    }
+
+    public void edit(){
+        System.out.println("Enter the Name to edit");
+        Scanner scan4 = new Scanner(System.in);
+        mTemp = scan4.nextLine();
+        System.out.println("1.Edit Name");
+        System.out.println("2.Edit Email");
+        System.out.println("3.Edit Phone Number");
+        System.out.println("3.Return to Phonebook");
+        System.out.println("5.");
+        switch(scan4.nextInt()){
+            case 1:{
+                break;
+            }
+            case 2:{
+//                break;
+            }
+            case 3:{
+//                break;
+            }
+            case 4:{
+//                break;
+            }
+        }
 
     }
 }
--- Run.java
+++ Run.java
@@ -1,6 +1,78 @@
-public class Run {
-    public static void main(String[] args) {
-        PhoneDiary run = new PhoneDiary();
-        run.run();
+import java.io.*;
+import java.util.HashMap;
+
+public class Run{
+    public static void main(String[] args) throws Exception {
+
+            PhoneDiary phoneDiary;
+//        try {
+//            File fi = new File("obj.txt");
+//            fi.createNewFile();
+//            FileInputStream fis = new FileInputStream(fi);
+//            ObjectInputStream ois = new ObjectInputStream(fis);
+//            phoneDiary = (PhoneDiary) ois.readObject();
+//        }
+//        catch(Exception e) {
+//            PhoneDiary phoneDiary = new PhoneDiary();
+//            System.out.println("Exception 1"+e);
+//            File f = new File("obj.txt");
+//            FileOutputStream fos = new FileOutputStream(f);
+//            ObjectOutputStream oos = new ObjectOutputStream(fos);
+//            PhoneDiary phoneDiary = (PhoneDiary) ois.readObject();
+////            oos.writeObject(new HashMap<String, Object>());
+//        }
+
+//        phoneDiary.run();
+//
+//        File f = new File("obj.txt");
+//        fi.createNewFile();
+//        FileOutputStream fos = new FileOutputStream(f);
+//        ObjectOutputStream oos = new ObjectOutputStream(fos);
+//        oos.writeObject(phoneDiary);
+            //phoneDiary.run();
+
+//        try{
+//            File f = new File("obj.txt");
+//            FileOutputStream fos = new FileOutputStream(f);
+//            ObjectOutputStream oos = new ObjectOutputStream(fos);
+//            //oos.writeObject(phoneDiary);
+//        }
+//        catch (Exception e){
+//            System.out.println(e);
+//        }
+
+
+//        PhoneDiary pd = new PhoneDiary();
+//        pd.run();
+//        File f = new File("obj.txt");
+//        FileOutputStream fos = new FileOutputStream(f);
+//        ObjectOutputStream oos = new ObjectOutputStream(fos);
+//        oos.writeObject(pd);
+
+//        File fi = new File("obj.txt");
+//        FileInputStream fis = new FileInputStream(fi);
+//        ObjectInputStream ois = new ObjectInputStream(fis);
+//        PhoneDiary phoneDiary = (PhoneDiary)ois.readObject();
+//
+//        phoneDiary.run();
+//
+//        File fii = new File("obj.txt");
+//        FileOutputStream fosi = new FileOutputStream(fii);
+//        ObjectOutputStream oosi = new ObjectOutputStream(fosi);
+//        oosi.writeObject(phoneDiary);
+
+
+        File fi = new File("obj.txt");
+        FileInputStream fis = new FileInputStream(fi);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        PhoneDiary phonebook = (PhoneDiary)ois.readObject();
+
+        phonebook.run();
+
+        File f = new File("obj.txt");
+        FileOutputStream fos = new FileOutputStream(f);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(phonebook);
+
     }
 }
